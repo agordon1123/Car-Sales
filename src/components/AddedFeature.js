7be367eb7a0f328582d9f8a27b229a842cf07171
Alex <agordon1123@gmail.com>
2019-08-14 16:58:12 -0400

    connected App to store

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { buyNewItem } from '../actions';
 
-const AddedFeature = props => {
+const AddedFeature = (props) => {
+  console.log(props)
+  const [featureState, setFeatureState] = useState([])
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
@@ -10,4 +15,10 @@ const AddedFeature = props => {
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    features: state.car.features
+  }
+}
+
+export default connect(mapStateToProps, {})(AddedFeature);
